9f33cafcd6b26fdfcba03a38a28e696e91072387
tvschaitanya <tvschaitanya@gmail.com>
2024-07-31 23:40:18 -0400

    completed basics of js

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144554;
+let accoutEmail = "chai@gmail.com";
+var accountPasword = "12345";
+accountCity = "Jaipur";
+let accountState;
+
+// accountId = 2;
+console.log(accountId);
+
+accoutEmail = "hd@fc.com";
+accountPasword = "212121";
+accountCity = "Hyd";
+
+console.table([accountId, accoutEmail, accountPasword, accountCity]);
+console.log(accountState);
+
+//Notes
+/* Don't use var
+ because of issue in block scope and functional scope
+*/
