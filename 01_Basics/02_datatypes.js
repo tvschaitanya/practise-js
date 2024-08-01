4573e8869c469afb80c53917587191d23e81afe1
tvschaitanya <tvschaitanya@gmail.com>
2024-08-01 12:01:55 -0400

    adding new file

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treating all the code in new JS Version
+
+// alert(3 + 3); //diff. syntax in node, it will work in browser
+
+console.log(3 + 3);
+console.log(3 + 3);
+
+let name = "Chai";
+let age = 26;
+let isLoggedIn = false;
+
+// Notes:
+/*
+1. Code should be readable
+*/
+
+// number =>2 to power 53
+// bigint - very big number, real world usage in trading
+// string => " "
+// boolean => true / false
+// null => standalone value, represents empty (not assigned)
+// undefined => when value is not assigned
+// symbol => unique values
+
+// object
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
